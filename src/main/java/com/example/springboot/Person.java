f93d69e0a8bdc99ba6e523ca438a423215ce72c2
DESKTOP-SQM5DDS\Oana <oanaalexandra723@yahoo.com>
2022-07-13 11:04:15 +0300

    SPRING-006 Add fields in Person class

--- src/main/java/com/example/springboot/Person.java
+++ src/main/java/com/example/springboot/Person.java
@@ -5,6 +5,40 @@ public class Person {
     private int age;
     private String sex;
 
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+
+    public Person(String lastName, String address, String phoneNumber) {
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public Person() {
         System.out.println("Created a null person.");
     }
